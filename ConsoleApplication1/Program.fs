904c7e6cd8b9d2839a7d3a9fa1dd53cfd069927f
EmiliyanG <Emiliyang@illycorp.com>
2017-11-12 16:13:27 +0000

    1 - Improve logging by quoting source and destination file paths
    2 - Provide /Close parameter while running TeraCopy
    3 - Delete files in destination directory only if the file to copy from the source folder does not exist in the destination folder

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -12,10 +12,11 @@ open System.Collections.Generic
 let mutable errorsList = new List<string>()
 
 type myFile = {filePath:string; dateCreated:DateTime}
-
 //function to return the current date time 
 let getTime() =  ( System.DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")) + " > " 
-    
+
+let (^) l r = sprintf "%s%s" l r
+
 //get IO.File last date modified
 let getDateModified filePath =
     let myFile = System.IO.FileInfo(filePath)
@@ -53,7 +54,7 @@ let copyFile clientName teraCopyExe sourceFile destination=
     |true, true ->
         
         //skip file if already exists in the destination folder
-        let command = "Copy \""+sourceFile+"\" \""+destination+"\" /SkipAll"
+        let command = "Copy \""+sourceFile+"\" \""+destination+"\" /SkipAll /Close"
         
         printfn "%s started copying file \"%s\" to \"%s\"" (getTime()) sourceFile destination
         
@@ -65,7 +66,7 @@ let copyFile clientName teraCopyExe sourceFile destination=
         match myProcess.HasExited with
         | true -> printfn "%s file copied successfully " (getTime()) 
         | false -> myProcess.Kill()
-                   errorsList.Add ( getTime()+"processing client: "+clientName+ 
+                   errorsList.Add ( getTime()+" processing client: "+clientName+ 
                                     ". Copying process has failed for file \""+sourceFile+"\"."+
                                     "\nThe process had to be killed. Possible reasons can be:\n timeout; "+
                                     "\nanother process was trying to run teracopy in the same time." )
@@ -75,12 +76,12 @@ let copyFile clientName teraCopyExe sourceFile destination=
             printfn "%s process had to be terminated exitCode is %i " (getTime())  myProcess.ExitCode
             
     | true, false -> 
-        printfn "%s Tried copying file %s but the file does not exist" (getTime()) sourceFile
-        errorsList.Add ( getTime()+"processing client: "+clientName+"."+
+        printfn "%s Tried copying file \"%s\" but the file does not exist" (getTime()) sourceFile
+        errorsList.Add ( getTime()+" processing client: \""+clientName+"\"."+
                                     "\nTried copying file \""+sourceFile+"\" but the file does no longer exists in the specified directory.")
     |false, _ -> 
-        printfn "%s Tried copying file %s but the TeraCopy.exe path \"%s\" is invalid" (getTime()) sourceFile teraCopyExe
-        errorsList.Add ( getTime()+"processing client: "+clientName+"."+
+        printfn "%s Tried copying file \"%s\" but the TeraCopy.exe path \"%s\" is invalid" (getTime()) sourceFile teraCopyExe
+        errorsList.Add ( getTime()+" processing client: "+clientName+"."+
                                     "\nTried copying file \""+sourceFile+"\" but the TeraCopy.exe path \""+teraCopyExe+"\" is invalid.")
 
 let (|Int|_|) str =
@@ -93,7 +94,7 @@ let deletePreviousFiles deleteFiles dir ext =
     match deleteFiles with
     //delete only files older than x hours
     | Some (Int x) -> 
-        printfn "%s deleting files older than %i hours for directory %s" (getTime()) x dir
+        printfn "%s deleting files older than %i hours for directory \"%s\"" (getTime()) x dir
         match getfilesForDirectory dir ext with
         | Some files -> 
             files 
@@ -120,18 +121,25 @@ let processFile clientName teraCopyExe deleteFiles (f:xFile) =
     | Some ext, Some src, Some dest -> 
         if not (Directory.Exists dest) 
             then 
-                printfn "%s creating destination directory %s" (getTime()) dest
+                printfn "%s creating destination directory \"%s\"" (getTime()) dest
                 Directory.CreateDirectory dest |> ignore
             
-        deletePreviousFiles deleteFiles dest ext
 
         match (getMostRecentFile src ext) with
-        |Some x -> printfn "%s The most recently modified file is %s " (getTime()) (Path.GetFileName(x.filePath))
-                   copyFile clientName teraCopyExe x.filePath dest
-        |None -> printfn "%s no files found for directory %s" (getTime()) src
+        |Some x -> let fileName = Path.GetFileName(x.filePath)
+                   printfn "%s The most recently modified file in directory \"%s\" is \"%s\" " (getTime()) src fileName
+                   
+                   
+                   match File.Exists (dest ^ "\\" ^ fileName ) with 
+                   | true -> printfn "%s Skipping file \"%s\". The file already exists in directory \"%s\"" (getTime()) (Path.GetFileName(x.filePath)) dest
+                   | false ->
+                       deletePreviousFiles deleteFiles dest ext
+                       copyFile clientName teraCopyExe x.filePath dest
+
+        |None -> printfn "%s no files found for directory \"%s\"" (getTime()) src
     | _, _, _-> 
         printfn "%s file could not be processed due to invalid format in the XML file" (getTime())
-        errorsList.Add ( getTime()+"processing client: "+clientName+"."+
+        errorsList.Add ( getTime() ^ " processing client: \"" ^ clientName ^ "\"." ^
                                     "\nFile tag in the clients.xml file has invalid format and could not be processed.")
         
        
@@ -145,7 +153,7 @@ let processClient teraCopyExe deleteFiles client =
         //check if there are no files against the specified client
         if Seq.isEmpty client.filesToBackup then 
             printfn "%s no files found for client %s" (getTime()) n
-            errorsList.Add ( getTime()+"processing client: "+n+"."+
+            errorsList.Add ( getTime()+" processing client: "+n+"."+
                "\nNo files to backup specified in the clients.xml file.")
 
         else client.filesToBackup |> Seq.iter (processFile n teraCopyExe deleteFiles)
@@ -174,7 +182,7 @@ let emailErrors (errorsList:List<String>) (mailSettings:xEmail) =
             "<table><tbody>"+
             errors +
             "</table></tbody>"+
-            "<br /><p>Please fix this issues.</p>"
+            "<br /><p>Please fix these issues.</p>"
         ()
         sendEmail mailSettings msg |> ignore
     
@@ -189,28 +197,17 @@ let main argv =
         | xs -> xs |> parseCommandLine defaultOptions 
             
 
-    match m.xmlPath with
-    |Some p -> 
-        match (loadConfigs p) with
-        | Some configs -> 
-            match configs.smtpServer, configs.smtpServerPort with
-            | Some smtp, Some port -> 
-                sendEmail configs "test email" |> ignore
-            | Some smtp, none -> printfn "no port provided for smtpServer: %s" smtp
-            | _,_ -> printfn "no smtp server provided. Attempting localhost"
-        | None -> printfn "no configs found"
-    | _ -> printfn "no xmlPath provided"
-    
-    
+    match m.xmlPath, m.teraCopy with
+    | Some x, Some y -> 
+        let clients = extractedClients x 
+        processClients y m.deleteFiles clients
 
-    //match m.xmlPath, m.teraCopy with
-    //| Some x, Some y -> 
-    //    let clients = extractedClients x 
-    //    processClients y m.deleteFiles clients
-    //    emailErrors errorsList
-    //    //sendEmailTest "smtp.gmail.com" msg
-    //| _ , _ -> printfn "no appropriate params provided"
+        match (loadConfigs x) with
+        | Some mailSettings -> 
+           emailErrors errorsList mailSettings
+        | None -> printfn "no email settings were provided. Unable to send email."
 
-    //sendEmailTest "80.175.22.70" "test email"
+    | _ , _ -> printfn "no appropriate command line params provided"
+    
     
     0 // return an integer exit code 
